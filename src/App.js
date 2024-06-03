c9db8451d3af1dfe906058da36e2dec6c2e9e355
prasath111998 <ajay15215804@gmail.com>
2024-06-03 19:17:37 +0530

    Name Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component{
   render(){
     return(
       <div>
-        <h1> Hello </h1>
+        <h1> Hello  Vignesh</h1>
         <p>Welcome to React Class</p>
       </div>
     )
