b6ff6ec22ac30d8b83121cfb6260acaa9ddacf1c
prasath111998 <ajay15215804@gmail.com>
2024-06-03 18:44:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, {Component} from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1> Hello </h1>
+        <p>Welcome to React Class</p>
+      </div>
+    )
+  }
+}
+
+export default App;
